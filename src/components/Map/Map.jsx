1bfb9f70206f2961c7c78014b4b899d72b0d565c
Ayan Bag <ayanbag9474@gmail.com>
2020-07-21 21:32:26 +0530

    Mapbox Token and Style Updated

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -37,7 +37,7 @@ const Map = ({ data }) => {
           {...viewport}
           width='100%'
           height='100%'
-          mapStyle="mapbox://styles/ayanbag/ck9r7hmth1pj31imromhcamlc"
+          mapStyle="mapbox://styles/ayanbag/ck9r7e2py1pfs1imr18xijrsl"
           onViewportChange={updateViewport}
           mapboxApiAccessToken={TOKEN}
         >
